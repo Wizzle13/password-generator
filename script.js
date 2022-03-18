344c2a42587a92ab5c88e8953ce840b7e17d246b
CBurton <burton.chris13@gmail.com>
2022-03-18 18:36:48 -0500

    add information collection and restricts avalible anwsers

--- script.js
+++ script.js
@@ -1,15 +1,106 @@
 // Assignment code here
+var charCount = "";
+var upperCase = "";
+var lowerCase = "";
+var numeric = "";
+var special = "";
 
-function generatePassword(){
-  charCount = prompt("How many characters would you like your password? (8-128");
-  console.log(charCount);
+// This section will get the lenght of the password and make sure it is between 8-128.
+function getCharCount(){
+  charCount = prompt("How many characters would you like your password? (8-128)");
+  charCount = charCount.toLowerCase();
+  if (charCount >= 8 && charCount <= 128){
+    return charCount;
+  } 
+  else {
+    window.alert("Youn number need to be bettween 8 - 128!");
+    getCharCount();
+  } 
+  
+}
+
+//This section will determan it you want Uppercase
+function getUpperCase(){
   upperCase = prompt("Would you like UPPER case letters? (yes or no)");
-  console.log(upperCase);
+  upperCase = upperCase.toLocaleLowerCase();
+  if (upperCase === "" || upperCase === null){
+    window.alert("You need to anwser Yes of No");
+    getUpperCase();
+  }
+  if (upperCase === "yes" || upperCase === "no"){
+    return upperCase;
+  }
+  else {
+    window.alert("You need to anwser Yes of No");
+    getUpperCase();
+  }
+  
+}
+
+//This section will determan it you want lowercase
+function getLowerCase(){
   lowerCase = prompt("Would you like lower case letters? (yes or no)");
-  console.log(lowerCase);
-  numeric = prompt("Would you like to use numerics? (yes or no)");
-  console.log(numeric);
+  lowerCase = lowerCase.toLocaleLowerCase();
+  if (lowerCase === "" || lowerCase === null){
+    window.alert("You need to anwser Yes of No");
+    getlowerCase();
+  }
+  if (lowerCase === "yes" || lowerCase === "no"){
+    return lowerCase;
+  }
+  else {
+    window.alert("You need to anwser Yes of No");
+    getLowerCase();
+  }
+  
+}
+
+//This section will determian it you want numerics
+function getNumeric(){
+  numeric = prompt("Would you like numerics? (yes or no)");
+  numeric = numeric.toLocaleLowerCase();
+  if (numeric === "" || numeric === null){
+    window.alert("You need to anwser Yes of No");
+    getNumeric();
+  }
+  if (numeric === "yes" || numeric === "no"){
+    return numeric;
+  }
+  else {
+    window.alert("You need to anwser Yes of No");
+    getNumeric();
+  }
+  
+}
+
+//This section will determian it you want Special Characters
+function getSpecial(){
   special = prompt("Would you like to use Special Characters? (yes or no)");
+  special = special.toLocaleLowerCase();
+  if (special === "" || special === null){
+    window.alert("You need to anwser Yes of No");
+    getSpecial();
+  }
+  if (special === "yes" || special === "no"){
+    return special;
+  }
+  else {
+    window.alert("You need to anwser Yes of No");
+    getSpecial();
+  }
+  
+}
+// This section will generate the password.
+function generatePassword(){
+  getCharCount();
+  console.log(charCount);
+  getUpperCase();
+  console.log(upperCase);
+  getLowerCase();
+  console.log(lowerCase);
+  getNumeric();
+  console.log(numeric);
+  getSpecial();
   console.log(special);
 } 
 
